9abfcc1c3e9e3d4784dd3a469388919fd111d7df
MonroeLionel <MonroeLionel@gmail.com>
2022-08-02 18:12:25 +0300

    7 начало

--- src/AddItemForm.tsx
+++ src/AddItemForm.tsx
@@ -1,45 +1,47 @@
-import React, {ChangeEvent, KeyboardEvent, useState} from "react";
+import React, {ChangeEvent, KeyboardEvent, useState} from 'react';
 
-type addItemFormPropsTpe = {
-   addItem: (title: string) => void
+type AddItemFormPropsType = {
+    addItem: (title: string) => void
 }
 
-export function AddItemForm(props: addItemFormPropsTpe) {
-   const errorMessageStyles = {color: "hotpink"}
-   const [title, setTitle] = useState("")
-   const [error, setError] = useState<boolean>(false)
+const AddItemForm: React.FC<AddItemFormPropsType> = (props) => {
+    const [title, setTitle] = useState("")
+    const [error, setError] = useState<boolean>(false)
 
 
-   const onChangeSetTitle = (e: ChangeEvent<HTMLInputElement>) => {
-      error && setError(false)
-      setTitle(e.currentTarget.value)
-   }
+    const onChangeSetTitle = (e: ChangeEvent<HTMLInputElement>)=> {
+        error && setError(false)
+        setTitle(e.currentTarget.value)
+    }
+    const onKeyDownAddItem = (e: KeyboardEvent<HTMLInputElement>) => {
+        if(e.key === "Enter" && e.ctrlKey === true){
+            onClickAddItem()
+        }
+    }
+    const onClickAddItem = () => {
+        const trimmedTitle = title.trim()
+        if(trimmedTitle){
+            props.addItem(trimmedTitle)
+        } else {
+            setError(true)
+        }
+        setTitle("")
+    }
 
-   const onKeyDownAddItem = (e: KeyboardEvent<HTMLInputElement>) => {
-      if (e.key === "Enter" && e.ctrlKey) {
-         onClickAddItem()
-      }
-   }
-   const onClickAddItem = () => {
-      const trimmedTitle = title.trim()
-      if (trimmedTitle) {
-         props.addItem(trimmedTitle)
-      } else {
-         setError(true)
-      }
-      setTitle("")
-   }
+    const errorMessageStyles = {color: "hotpink"}
 
-   return (
-     <div>
-        <input
-          value={title}
-          onChange={onChangeSetTitle}
-          onKeyDown={onKeyDownAddItem}
-          className={error ? "error" : ""}
-        />
-        <button onClick={onClickAddItem}>+</button>
-        {error && <div style={errorMessageStyles}>Title is required!</div>}
-     </div>
-   )
-}
+    return (
+        <div>
+            <input
+                value={title}
+                onChange={onChangeSetTitle}
+                onKeyDown={onKeyDownAddItem}
+                className={error ? "error" : ""}
+            />
+            <button onClick={onClickAddItem}>+</button>
+            {error && <div style={errorMessageStyles}>Title is required!</div>}
+        </div>
+    );
+};
+
+export default AddItemForm;
--- src/EditableSpan.tsx
+++ src/EditableSpan.tsx
@@ -1,31 +1,31 @@
-import React, {ChangeEvent, useState} from "react";
+import React, {ChangeEvent, useState} from 'react';
 
 type EditableSpanPropsType = {
-   title: string
-   changeTitle: (newTitle: string) => void
+    title: string
+    changeTitle: (newTitle: string)=> void
 }
 
-export function EditableSpan(props: EditableSpanPropsType) {
-   const [editMode, setEditMode] = useState<boolean>(false)
-   const [title, setTitle] = useState<string>(props.title)
+const EditableSpan: React.FC<EditableSpanPropsType> = (props) => {
+    const [editMode, setEditMode] = useState<boolean>(false)
+    const [title, setTitle] = useState<string>(props.title)
+    const onChangeSetTitle = (e: ChangeEvent<HTMLInputElement>)=> {
+        setTitle(e.currentTarget.value)
+    }
+    const onEditMode = () => setEditMode(true)
+    const offEditMode = () => {
+        props.changeTitle(title)
+        setEditMode(false)
+    }
+    return (
+        editMode
+        ? <input
+            value={title}
+            onChange={onChangeSetTitle}
+            autoFocus
+            onBlur={offEditMode}
+            />
+        : <span onDoubleClick={onEditMode}>{props.title}</span>
+    );
+};
 
-   const onChangeSetTitle = (e: ChangeEvent<HTMLInputElement>) => {
-      setTitle(e.currentTarget.value)
-   }
-   const onEditMode = () => setEditMode(true)
-   const offEditMode = () => {
-      setEditMode(false)
-      props.changeTitle(title)
-   }
-
-   return (
-     editMode ? <input
-         type="text"
-         value={title}
-         onChange={onChangeSetTitle}
-         autoFocus={true}
-         onBlur={offEditMode}
-       /> :
-       <span onDoubleClick={onEditMode}>{props.title}</span>
-   )
-}
+export default EditableSpan;
